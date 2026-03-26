1159bfe78ef9d3da35beb42300fbd3dd0e6545a0
pyr <jzhangnv@gmail.com>
2026-03-25 19:36:42 -0700

    Added component to display images

---
+++ src/CardList.jsx
@@ -0,0 +1,38 @@
+import {useState, useEffect} from 'react'
+import {fetchImageList, randomize} from './util.js';
+
+export default function CardList({setScore, score}) {
+  const [imageList, setImageList] = useState([]);
+  const [key, setKey] = useState(false);
+  const [clickedList, setClickedList] = useState([]);
+  
+  useEffect(() => {
+      fetchImageList(5).then(result => {
+        setImageList(result);
+      });
+  }, []);
+  
+  function handleClick(id) {
+    if (clickedList.indexOf(id) > -1) {
+      setClickedList([]);
+      setScore(0);
+    } else {
+      const list = clickedList.slice();
+      list.push(id);
+      setClickedList(list);
+      setImageList(randomize(imageList));
+      setScore(score + 1);
+      setKey(!key);
+    }
+  };
+
+ return (
+ <ul className="cardList" key={key}>
+  {imageList.map((imageProfile) => 
+    <li onClick={() => handleClick(imageProfile.id)} className="imageCard" key={imageProfile.id}>
+      <img src={imageProfile.url} />
+      <p>{imageProfile.title}</p>
+    </li>
+  )}
+ </ul>); 
+}
